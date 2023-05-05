71e93326bd8d41b7f06c1e8eb6c90c520e07f024
j-torrez <juanboscotorrez@gmail.com>
2023-05-05 15:27:00 +0200

    isalnum function fixed

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,1 +1,28 @@
-/* Check function Comment*/
+#include <stdio.h>
+
+int isalnum (int c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z') || (c >= '0' && c <= '9'))
+    {
+        return(1);
+    }
+    else 
+    {
+        return(0);
+    }
+}
+
+int main(void)
+{
+    unsigned char c;
+
+    c = '4';
+    if (isalnum(c))
+    {
+        printf("%c, Is alphanum", c);
+    }
+    else
+    {
+        printf("%c, NOT alphanum", c);
+    }
+}
