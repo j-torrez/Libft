0376aaef4acaeadf708dcc145d569382c945a245
j-torrez <juanboscotorrez@gmail.com>
2023-05-07 19:25:35 +0200

    Syntax changes

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,7 +3,10 @@
 
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
-    size_t  i; 
+    size_t  i;
+
+    if (!dest && !src)
+        return(0); 
 
     i = 0; 
     while(i < n)
--- memmove.c
+++ memmove.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-
 void *memmove(void *dest, const void *src, size_t n)
-{
+{   
+    char *source;
+    char *destination;
+    size_t  i;
+
+    source = (char *)src;
+    destination = (char *)dest;
+    i = 0;
+
+    if (destination > source )
     
+    while (i < n)
+    {
+        destination[i] = source[i];
+        i++;
+    }
+    return (dest);
 }
