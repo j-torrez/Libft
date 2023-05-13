5d19a5d5aa4272ada5482ee97d614f59d82e92c1
Juan Bosco Torrez Castillo <johnbosco@Juans-MacBook-Air.local>
2023-05-13 13:15:26 +0200

    Fixed functions

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -26,12 +26,8 @@ char	*ft_strrchr(const char *s, int c)
 		if (s[i] == c)
 		{
 			return ((char *)s + i);
-			i--;
 		}
-	}
-	if (c == '\0')
-	{
-		return ((char *)s + i);
+		i--;
 	}
 	return (0);
 }
