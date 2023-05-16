913f4b1f7ed1f7e5ad1c0cb1b0e64d1c89ab42fc
Juan Bosco Torrez Castillo <juanboscotorrez@gmail.com>
2023-05-16 02:17:08 +0200

    Adding ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,35 @@
+                                                
+#include "libft.h"
+
+/* *s: A pointer to a constant string
+    *f: A pointer to a function (char (*f) which takes
+    an unsigned int and a char as a arguments and return
+    char*/
+
+/*
+    1.Convert character, adding number according to the index.
+    -Add THE ASCII value of characther to
+    -char in function F take a character from S and transform that
+    character into a new character
+    -Store the new character in a new string
+    -Return the new string
+*/ 
+char *ft_strmapi (char const *s, char (*f)(unsigned int, char))
+{
+    size_t  i;
+    char *str;
+
+    str = (char *) malloc(sizeof(char) * ft_strlen(s) + 1);
+    if (!str)
+    {
+        return(0);
+    }
+    i = 0; 
+    while (s[i] != '\0')
+    {
+        str[i] = f(i, s[i]);
+        i++;
+    }
+    str[i] = '\0';
+    return str;
+}
