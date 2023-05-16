e61ef9bdaa127c8df6dd35b7d0723c2566b87841
Juan Bosco Torrez Castillo <johnbosco@Juans-MacBook-Air.local>
2023-05-16 18:26:52 +0200

    adding striteri function

---
+++ ft_striteri.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+/* Modify char base on it index, you can also say *str = *str + index
+void    modifychar(unsigned int index, char *str)
+{
+    str[index] = str[index] + index;
+}   */
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+    size_t  i;
+
+    i = 0;
+    
+    while (s[i] != '\0')
+    {
+        f(i, &s[i]);
+        i++;
+    }
+}
+/*
+int main(void)
+{
+    char str[] = "Hello";
+    printf("Before modify %s\n", str);
+    ft_striteri(str, modifychar);
+    printf("After: %s\n", str);
+    return 0; 
+}   */
