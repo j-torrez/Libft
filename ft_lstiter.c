f2bdf2b04a2b964402c60928de0cdddc92a2c19a
Juan Bosco Torrez Castillo <juanboscotorrez@gmail.com>
2023-05-19 03:22:06 +0200

    adding more functions

---
+++ ft_lstiter.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+/*
+    *lst: The addesss of a pointer to a node.
+    f: The address of the function used to iterate on the list
+*/
+/* if you use (ptr != 0) the last node will be processed
+    if you use (ptr -> next != 0) the last node will be skipped*/
+
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+    t_list *ptr;
+
+    ptr = lst;
+    if (!lst)
+        return;
+    while (ptr != 0)
+    {
+        f(ptr->content);
+        ptr = ptr->next;
+    }
+}
--- ft_split.c
+++ ft_split.c
@@ -12,18 +12,54 @@
 
 #include "libft.h"
 #include <stdio.h>
+/* *count -> numbers of substring are
+	*count = 0, we are dereferencing the pointer
+	to actually set the value that is pointing to 0*/
 
+/*
 char **ft_split(char const *s, char c)
 {
-	int len;
-	int i;
+	int *count;
+	size_t	len;
+	size_t	i;
 
 	len = ft_strlen(s);
+	*count = 0;
 	i = 0;
-}
+	while (i < len)
+	{
+		while (i < len)
+		{
+			if(s[i] == c)
+				i++;
+		}
+	}
+}*/
 
-int	main(void)
+static int count_substr(char *s, char c)
 {
-	
+	size_t i;
+	size_t count;
+
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != c && s[i])
+			count++;
+		while (s[i] != c && s[i])
+			i++;
+	}
+	return (count);
 }
 
+int main(void)
+{
+	int result;
+	char s[] = " He lo Wo rld How ds sd s sdsd sdsd sd";
+	result = count_substr(s, ' ');
+	printf("Total substring is: %d\n", result);
+	return (0);
+}
