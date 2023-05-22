f3bf0a61395e4cdcb72ae66937b27c0a4cc86d4a
Juan Bosco Torrez Castillo <juanboscotorrez@gmail.com>
2023-05-22 14:30:00 +0200

    fix memory issue

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -12,23 +12,26 @@
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+static void ft_putchar(char c, int fd)
 {
-	long	nb;
+	write(fd, &c, 1);
+}
+
+void ft_putnbr_fd(int n, int fd)
+{
+	long nb;
 
 	nb = n;
 	if (nb < 0)
 	{
-		nb = -nb;
-		ft_putchar_fd('-', fd);
+		nb *= -1;
+		ft_putchar('-', fd);
 	}
-	if (nb >= 10)
+	if (nb > 9)
 	{
 		ft_putnbr_fd(nb / 10, fd);
 		ft_putnbr_fd(nb % 10, fd);
 	}
 	else
-	{
-		ft_putchar_fd((nb + '0'), fd);
-	}
+		ft_putchar(nb + 48, fd);
 }
